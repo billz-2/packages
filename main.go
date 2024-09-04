bd95ed8a269adb6a62af3c9d864e0fee5f683ff7
Akbar <akbar.xasanov.2017@gmail.com>
2024-09-04 15:44:24 +0500

    fix

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/billz-2/packages/pkg/bug_notifier"
+import "github.com/billz-2/packages/bug_notifier"
 
 func main() {
 	_ = bug_notifier.NewBugsnag(bug_notifier.Config{})
