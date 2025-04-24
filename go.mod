475c50942597f8bfcb37c8d2a8821b20a90b1603
Vadim Zakharyan <vadim.zakharyan@billz.uz>
2025-04-24 11:58:45 +0500

    fix: tracing rollback to prev implementation

--- go.mod
+++ go.mod
@@ -19,7 +19,7 @@ require (
 	github.com/stretchr/testify v1.10.0
 	github.com/testcontainers/testcontainers-go v0.36.0
 	go.opentelemetry.io/otel v1.35.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.34.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.17.0
 	go.opentelemetry.io/otel/sdk v1.34.0
 	go.opentelemetry.io/otel/trace v1.35.0
 	go.uber.org/mock v0.5.0
@@ -102,9 +102,7 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.34.0 // indirect
 	go.opentelemetry.io/otel/metric v1.35.0 // indirect
-	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
--- pkg/tracing/tracer.go
+++ pkg/tracing/tracer.go
@@ -1,14 +1,12 @@
 package tracing
 
 import (
-	"context"
-
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -27,40 +25,44 @@ func GetGlobalTracer() trace.Tracer {
 }
 
 func init() {
-	_, _ = NewTraceProvider(context.Background(), &Config{})
+	_, _ = NewTraceProvider(&Config{})
 }
 
-func NewTraceProvider(ctx context.Context, jaegerConfig *Config) (*sdktrace.TracerProvider, error) {
-	exp, err := newExporter(ctx, jaegerConfig)
+func newExporter(jaegerConfig *Config) (*jaeger.Exporter, error) {
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(jaegerConfig.JaegerUrl)))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create the Jaeger exporter")
+	}
+
+	return exporter, nil
+}
+
+func newTraceProvider(exp sdktrace.SpanExporter, jaegerConfig *Config) (*sdktrace.TracerProvider, error) {
+	res := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(jaegerConfig.ServiceName),
+	)
+
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(res),
+	), nil
+}
+
+func NewTraceProvider(jaegerConfig *Config) (*sdktrace.TracerProvider, error) {
+	exp, err := newExporter(jaegerConfig)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to initialize exporter")
 	}
 
-	res := newResource(jaegerConfig)
-
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(res),
-	)
+	tp, err := newTraceProvider(exp, jaegerConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create the trace provider")
+	}
 
 	otel.SetTracerProvider(tp)
 
 	tracer = tp.Tracer(jaegerConfig.ServiceName)
 
 	return tp, nil
-}
-
-func newExporter(ctx context.Context, jaegerConfig *Config) (sdktrace.SpanExporter, error) {
-	return otlptracegrpc.New(
-		ctx,
-		otlptracegrpc.WithEndpointURL(jaegerConfig.JaegerUrl),
-		otlptracegrpc.WithInsecure(),
-	)
-}
-
-func newResource(jaegerConfig *Config) *resource.Resource {
-	return resource.NewWithAttributes(
-		semconv.SchemaURL,
-		semconv.ServiceNameKey.String(jaegerConfig.ServiceName),
-	)
 }
