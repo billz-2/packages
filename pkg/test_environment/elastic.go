8fe02984153aef69a85b46bb3eb67c30f2d4c09a
Davron <idavronkholmatov@gmail.com>
2025-11-24 18:52:48 +0500

    fix: streamline Elasticsearch plugin installation and remove unnecessary logging

--- pkg/test_environment/elastic.go
+++ pkg/test_environment/elastic.go
@@ -24,7 +24,6 @@ func SetupElastic(ctx context.Context, cfg Config) (esConfig elasticsearch.Confi
 
 	exposedPort, err := GetFreePort()
 	if err != nil {
-		logger.Log.Error("Could not get free port", logger.Error(err))
 		return esConfig, nil, err
 	}
 
@@ -56,26 +55,19 @@ func SetupElastic(ctx context.Context, cfg Config) (esConfig elasticsearch.Confi
 		return elasticsearch.Config{}, nil, err
 	}
 
-	logger.Log.Info("Installing analysis-icu plugin...")
-	exitCode, output, err := elastic.Exec(ctx, []string{"elasticsearch-plugin", "install", "-b", "analysis-icu"})
+	exitCode, output, err := elastic.Exec(ctx, []string{"elasticsearch-plugin", "install", "analysis-icu"})
 	if err != nil || exitCode != 0 {
-		logger.Log.Error("Failed to install plugin", logger.Error(err), logger.Any("output", output))
 		return elasticsearch.Config{}, nil, fmt.Errorf("failed to install plugin: %v, output: %s", err, output)
 	}
-	logger.Log.Info("Plugin installed successfully")
 
 	logger.Log.Info("Restarting Elasticsearch...")
 	if err := elastic.Stop(ctx, nil); err != nil {
-		logger.Log.Error("Failed to stop container", logger.Error(err))
 		return elasticsearch.Config{}, nil, err
 	}
 
 	if err := elastic.Start(ctx); err != nil {
-		logger.Log.Error("Failed to start container", logger.Error(err))
 		return elasticsearch.Config{}, nil, err
 	}
-
-	logger.Log.Info("Waiting for Elasticsearch to be ready...")
 
 	ip, err := elastic.Host(ctx)
 	if err != nil {
