59b70eb902d386464bfde0276d2347ddbf41b3d5
Davron <idavronkholmatov@gmail.com>
2025-11-24 14:09:33 +0500

    fix: update Elasticsearch container configuration and improve error handling

--- pkg/test_environment/elastic.go
+++ pkg/test_environment/elastic.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/billz-2/packages/pkg/logger"
 
-	"github.com/docker/go-connections/nat"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/google/uuid"
 	"github.com/testcontainers/testcontainers-go"
@@ -30,36 +29,38 @@ func SetupElastic(ctx context.Context, cfg Config) (esConfig elasticsearch.Confi
 	}
 
 	internalPort := 9200
-	ws := wait.ForHTTP("/").
-		WithPort(nat.Port(fmt.Sprintf("%d/tcp", internalPort))).
-		WithPollInterval(1 * time.Second).
+
+	ws := wait.ForListeningPort("9200/tcp").
 		WithStartupTimeout(5 * time.Minute)
 
 	req := testcontainers.ContainerRequest{
-
 		Image: "venomuz/elastic_analysis-icu:9.0.4",
 		Name:  "elastic-mock" + uuid.NewString(),
 		Env: map[string]string{
-			"discovery.type":         "single-node",
-			"ES_JAVA_OPTS":           "-Xms512m -Xmx512m",
-			"xpack.security.enabled": "false",
+			"discovery.type":                  "single-node",
+			"ES_JAVA_OPTS":                    "-Xms512m -Xmx512m",
+			"xpack.security.enabled":          "false",
+			"xpack.security.http.ssl.enabled": "false",
+			"http.host":                       "0.0.0.0",
+			"network.bind_host":               "0.0.0.0",
+			"network.publish_host":            "0.0.0.0",
 		},
 		ExposedPorts: []string{fmt.Sprintf("%d:%d/tcp", exposedPort, internalPort)},
-		//WaitingFor:   wait.ForLog("started"),
-		WaitingFor: ws,
-		AutoRemove: true,
+		WaitingFor:   ws,
 	}
 	elastic, err = testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
 		Started:          true,
 	})
 	if err != nil {
-		return elasticsearch.Config{}, nil, err
+		return elasticsearch.Config{}, nil, fmt.Errorf("failed to start elasticsearch container: %w", err)
 	}
+
+	time.Sleep(3 * time.Second)
 
 	ip, err := elastic.Host(ctx)
 	if err != nil {
-		return elasticsearch.Config{}, nil, err
+		return elasticsearch.Config{}, nil, fmt.Errorf("failed to get container host: %w", err)
 	}
 
 	elasticAddress := fmt.Sprintf("http://%s:%d", ip, exposedPort)
